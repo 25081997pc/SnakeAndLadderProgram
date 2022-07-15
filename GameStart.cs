e5fd1ae142d520bb08e988fb91c8cba4be093d34
25081997pc <chavanpooja2508@gmail.com>
2022-07-15 21:24:18 +0530

    Player is at start position 0

---
+++ GameStart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+    public class GameStart
+    {
+        public static void GameIsStart()
+        {
+            int Player = 1;
+            int Player_Position = 0;
+
+            Console.WriteLine("Player Position is : " + Player_Position);
+        }
+    }
+}
