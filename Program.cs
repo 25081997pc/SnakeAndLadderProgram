d19b24f59692ba372d84daee06c8bb5c2e811b97
25081997pc <chavanpooja2508@gmail.com>
2022-07-15 21:03:38 +0530

    Greeting message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Simulate Snake And Ladder Game");
+        }
+    }
+}
